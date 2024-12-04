264054b3c37b16f9d201370956e3edf96547794e
PORT-GIT <margaretkairu21@gmail.com>
2024-12-04 17:00:42 +0300

    created a notes model and generics class-based create view with customizations

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
+from .models import Note
 
 #this serializer can be used to accept and return a new user
 class UserSerializer(serializers.ModelSerializer):
@@ -9,14 +10,25 @@ class UserSerializer(serializers.ModelSerializer):
         #tells Django that it should accept the password when creating a new user
         #but its should not return the password when giving info about the user
         #WRITE_ONLY makes sure that noone reads what the password is
-        extra_kwargs = {'password': {'write_only': True}}
+        extra_kwargs = {'password': {"write_only": True}}
 
-        #this is a function implements the method to create a new user
-        #the validated data is the fields in the serializers
-        def create(self, validated_data):
-            user = User.objects.create_user(**validated_data)
-            #once the data has been validated in the first line the line above
-            #will create the new user now
-            return user
+    #this is a function implements the method to create a new user
+    #the validated data is the fields in the serializers
+    def create(self, validated_data):
+        user = User.objects.create_user(**validated_data)
+        #once the data has been validated in the first line the line above
+        #will create the new user now
+        return user
+    
+
+class NoteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Note
+        fields = ('id', 'title', 'content', 'created_at', 'author')
+        #the READ_ONLY means that we should only read who is the author
+        #but the author will be set based on whoever created the note meaning noone can write
+        #and assign an author of their choosing
+        #OR IN SHORT THE AUTHOR IS SET BY THE BACKEND AND NOT ANYONE ELSE
+        extra_kwargs = {'author': {'read_only' : True}}  
         
     
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User
 from rest_framework import generics
-from .serializers import UserSerializer
+from .serializers import UserSerializer, NoteSerializer
 from rest_framework.permissions import IsAuthenticated, AllowAny
+from .models import Note
 
 
 # Create your views here.
@@ -15,4 +16,38 @@ class CreateUserView(generics.CreateAPIView):
     permission_classes = [AllowAny]
     #allows anyone to access this view even though not authenticated to create a new user
 
+#this view will list all the notes created by the user or create a note
+class NoteListCreate(generics.ListCreateAPIView):
+    serializer_class = NoteSerializer
+    #this ensures that the user is autheticated before creating note
+    permission_classes = [IsAuthenticated]
 
+    #this a method
+    #that has been used unlike the above view to get access to the REQUEST OBJECT below
+    # which specifies the user so that i can filter the notes created by that user
+    #ALSO I CANNOT VIEW THE NOTES WRITTEN BY SOMEONE ELSE ONLY MYSELF
+    def get_queryset(self):
+        #this will get a user that is authenticated and has the permission to interact with this view
+        user = self.request.user
+        #get the authenticated user object is retrieved using the line above
+        #the user is then used to filter the notes written by the retrieved user object
+        return Note.objects.filter(author=user)
+        #this will filter all the notes written by the called user
+
+    #this is to customize the way we will create a note
+    #IN SHORT OVERRIDING THE CREATE METHOD FOR A NOTE
+    def perform_create(self, serializer):
+        #this will pass the data through the serializer to check if its valid or not
+        #according to the fields of the models to make sure it is accurate
+        #IS_VALID MANUALLY CHECKS IF THE DATA IS VALID
+        if serializer.is_valid():
+            #if the data to create a new version of the note is valid the serializer saves the data
+            #which will make a new version of the note
+            #anything can be passed in the SAVE() but in this case i have added the author because in
+            #the serializer i specified the author to be read-only
+            #since the data cannot be passed automatically it has been manually added
+            serializer.save(author = self.request.user)
+
+        else:
+            print(serializer.errors)
+            
