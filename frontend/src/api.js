9f6b5b79bea169267a7feae4926f21068015b064
PORT-GIT <margaretkairu21@gmail.com>
2024-12-07 14:45:21 +0300

    created the api that will allow authorization header to be added to the system/user requests while checking the availability of access tokens in the local storage of the browser

---
+++ frontend/src/api.js
@@ -0,0 +1,42 @@
+//in this file i am creating an interceptor
+import axios from "axios"
+// axios sends network requests
+import { ACCESS_TOKEN } from "./constants"
+
+// this will intercept all requests that will be sent
+//and automatically add the correct headers to avoid writing multiple times in the code
+
+// using AXIOS the interceptor will check if the request has an access token
+// if it does it will automatically add to the token to the request
+
+const api = axios.create({
+    // i am passing an object that will specify the base url
+    // this will allow me to import anything specified in the .ENV file
+    // TO IMPORT ANYTHING FROM AN .ENV FILE TO JAVASCRIPT CODE IT NEEDS TO BEGIN
+    // WITH A VITE like below
+    baseURL: import.meta.env.VITE_API_URL
+    // now when i use the api(const) i will not have to specify the url just the path 
+    // e.g. http://localhost:8000/api/notes/
+})
+
+api.interceptors.request.use(
+    // below is an arrow function
+    (config) => {
+        // this function wil check for an access token in the local storage
+        // if i do it will add an authorization header to the request
+        const token = localStorage.getItem(ACCESS_TOKEN);
+        if (token) {
+            // if the local storage has a token below is how a JWT ACCESS TOKEN is passed
+            config.headers.Authorization = `Bearer ${token}`
+            // these will add authoriztion header to all requests
+            return config
+        }
+        (error) => {
+            return Promise.reject(error)
+        }
+
+    }
+)
+
+export default api
+// this means i can call on api rather than use axios to send all requests
---
+++ frontend/src/constants.js
@@ -0,0 +1,7 @@
+// this wil define the constants to be used inside the API.JS file
+
+// these are placed here so that local storage will be used to store the access and refresh token in the browser
+//and i would want to have a key that i can use to access in my local storage
+//so these key will help do that
+export const ACCESS_TOKEN = "access";
+export const REFRESH_TOKEN = "refresh";
